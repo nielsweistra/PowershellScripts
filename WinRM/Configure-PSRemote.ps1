ca30b54aa15760e8e225f3d3d674114635970f6c
Niels Weistra <niels.weistra@gmail.com>
2018-03-02 13:53:49 +0100

    Configure PSRemote (standalone)

---
+++ WinRM/Configure-PSRemote.ps1
@@ -0,0 +1,6 @@
+﻿Enable-PSRemoting -Force -SkipNetworkProfileCheck
+New-SelfSignedCertificate -DnsName $env:COMPUTERNAME -CertStoreLocation Cert:\LocalMachine\My
+$cert = $(Get-ChildItem -path cert:\LocalMachine\My|Where-Object {$_.Subject -eq "CN=$env:COMPUTERNAME"})
+New-Item -Path WSMan:\LocalHost\Listener -Transport HTTPS -Address * -CertificateThumbPrint $Cert.Thumbprint –Force
+Get-ChildItem WSMan:\Localhost\listener | Where -Property Keys -eq 'Transport=HTTP' | Remove-Item -Recurse
+Set-Item wsman:\localhost\client\trustedhosts * -Force

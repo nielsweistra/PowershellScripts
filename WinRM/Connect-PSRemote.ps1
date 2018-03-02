77f2fe7df8bb22b6e09ba10351e150e7b7f8ab5b
Niels Weistra <niels.weistra@gmail.com>
2018-03-02 13:54:44 +0100

    Connect to WinRM endpoint using SSL

---
+++ WinRM/Connect-PSRemote.ps1
@@ -0,0 +1,9 @@
+﻿$hostName="srvtstwin01" # example: "mywindowsvm.westus.cloudapp.azure.com"
+$winrmPort = "5986"
+
+# Get the credentials of the machine
+$cred = Get-Credential
+
+# Connect to the machine
+$soptions = New-PSSessionOption -SkipCACheck -SkipCNCheck
+Enter-PSSession -ComputerName $hostName -Port $winrmPort -Credential $cred -SessionOption $soptions -UseSSL

134d6a65b544cfd9a0bac9b80c8dee443a9ea444
Niels Weistra <niels.weistra@gmail.com>
2018-03-25 03:09:57 +0200

    Add firewall exception

--- WinRM/Configure-PSRemote.ps1
+++ WinRM/Configure-PSRemote.ps1
@@ -1,6 +1,7 @@
-﻿Enable-PSRemoting -Force -SkipNetworkProfileCheck
+Enable-PSRemoting -Force -SkipNetworkProfileCheck
 New-SelfSignedCertificate -DnsName $env:COMPUTERNAME -CertStoreLocation Cert:\LocalMachine\My
 $cert = $(Get-ChildItem -path cert:\LocalMachine\My|Where-Object {$_.Subject -eq "CN=$env:COMPUTERNAME"})
 New-Item -Path WSMan:\LocalHost\Listener -Transport HTTPS -Address * -CertificateThumbPrint $Cert.Thumbprint –Force
 Get-ChildItem WSMan:\Localhost\listener | Where -Property Keys -eq 'Transport=HTTP' | Remove-Item -Recurse
 Set-Item wsman:\localhost\client\trustedhosts * -Force
+New-NetFirewallRule -DisplayName "Allow WinRm HTTPS" -Direction Inbound -LocalPort 5986 -Action Allow -Profile Any -Protocol TCP
